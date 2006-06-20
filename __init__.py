a967d95fe2b7302ec573af4895004ecf5ee1a8fe
Gauthier Bastien <g.bastien@imio.be>
2006-06-20 09:59:20 +0000

    Added DirectoryView

--- __init__.py
+++ __init__.py
@@ -5,11 +5,16 @@ BelgianEidAuthPlugin
 from AccessControl.Permissions import add_user_folders
 from Products.PluggableAuthService.PluggableAuthService import registerMultiPlugin
 from BelgianEidAuthPlugin import BelgianEidAuthPlugin, manage_addBelgianEidAuthPlugin, manage_addBelgianEidAuthPluginForm
+from Products.CMFCore.DirectoryView import registerDirectory
+
+registerDirectory('skins', globals())
+registerDirectory('skins/BelgianEidAuthPlugin', globals())
 
 def initialize(context):
     """ Initialize the GMailAuthPlugin """
+    
     registerMultiPlugin(BelgianEidAuthPlugin.meta_type)
-
+    
     context.registerClass( BelgianEidAuthPlugin
                             , permission=add_user_folders
                             , constructors=( manage_addBelgianEidAuthPluginForm
