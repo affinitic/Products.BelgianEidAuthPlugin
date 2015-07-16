dafbce5becc307c2deb670aa029bfcbfd138631f
Jean-François Roche <jfroche@affinitic.be>
2015-07-16 22:05:18 +0200

    Eggify
    Create an egg out of the Products directory.
    Create setup.py, define dependencies
    TODO: correct metadata

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('HISTORY.txt').read()
+    + '\n')
+
+setup(name='Products.BelgianEidAuthPlugin',
+      version=version,
+      description="",
+      long_description=long_description,
+      classifiers=[
+          "Programming Language :: Python"
+      ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages(),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      extras_require={
+          'test': ['plone.testing', 'plone.app.testing', 'plone.api'],
+      })
